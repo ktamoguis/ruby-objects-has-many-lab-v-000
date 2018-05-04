0cf5ba59444680eee7041ac2141b7e48c50c4352
Karl Amoguis <ktamoguis@gmail.com>
2018-05-04 07:49:05 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accesor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -9,12 +11,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(posttitle)
     newpost = Post.new(posttitle)
     newpost.author = self
     add_post(newpost)
+  end
+
+  def self.post_count
+    @@post_count
   end
 
 end
